0cbd3eea6d0bcb4c2f3003e8e8587fde6f00d020
Sri Devi <srideviv1303@gmail.com>
2025-04-08 21:13:26 +0530

    share code

--- src/screens/home/Home.jsx
+++ src/screens/home/Home.jsx
@@ -1,24 +1,50 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Header from '../../Components/Header'
 import Container1 from '../../Components/Container1'
 import CareerObjective from '../../Components/CareerObjective'
-import Education from'../../Components/Education'
+import Education from '../../Components/Education'
 import Skills from '../../Components/Skills'
 
 const Home = () => {
 
+  const homeRef = useRef(null)
+  const aboutRef = useRef(null)
+  const skillsRef = useRef(null)
+  const educationRef = useRef(null)
 
+  const scrollTohome = () => {
+    homeRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  };
+
+  const scrollToAbout = () => {
+    aboutRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  };
+  const scrollToSkills = () => {
+    skillsRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  };
+
+  const scrollToEducation = () => {
+    educationRef.current?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  };
 
 
 
   return (
     <div>
-      <Header />
-      <Container1 />
-      <CareerObjective />
-      <Skills/>
-      <Education/>
-      <div style={{ height: 200 }}></div>
+      <Header scrollToHome={scrollTohome} scrollToAbout={scrollToAbout} scrollToSkills={scrollToSkills} scrollToEducation={scrollToEducation} />
+      <div id='home' ref={homeRef}>
+        <Container1 />
+      </div>
+      <div ref={aboutRef}>
+        <CareerObjective />
+      </div>
+      <div ref={skillsRef}>
+        <Skills />
+      </div>
+      <div ref={educationRef}>
+        <Education />
+      </div>
+   
     </div>
   )
 }
