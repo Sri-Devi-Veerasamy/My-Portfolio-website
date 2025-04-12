42ca10084e3d794a751644a7e2afb1069f0aa281
Iyappan-mani <iyappan.99mani@gmail.com>
2025-04-12 20:54:02 +0530

    final

--- src/Components/Container1.jsx
+++ src/Components/Container1.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Container1.css';
 import Typewriter from 'typewriter-effect';
-import { Button, Nav } from 'react-bootstrap';
+import { Button, Nav, Toast, ToastContainer } from 'react-bootstrap';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
 import { FaWhatsapp } from 'react-icons/fa';
 import { MdCall } from 'react-icons/md';
@@ -9,6 +9,24 @@ import { FiDownload } from 'react-icons/fi';
 
 
 const Container1 = () => {
+    const [showToast, setShowToast] = useState(false);
+
+    const handleDownload = () => {
+        const link = document.createElement('a');
+        link.href = '/sri.pdf'; // path relative to the public folder
+        link.download = 'Sridevi.pdf'; // filename for download
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        setShowToast(true);
+    };
+
+    const handleClick = () => {
+        const phoneNumber = '+917339314128';
+        const message = 'Hi, I saw your portfolio and I\'m interested in working with you.';
+        const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
+        window.open(url, '_blank');
+      };
 
     return (
         <div
@@ -22,6 +40,7 @@ const Container1 = () => {
                 background: 'linear-gradient(to right, #BBF7D1, #BFDCFE)',
             }}
         >
+
 
             <div
                 className='text-container text-sm-center text-lg-start'
@@ -48,10 +67,10 @@ const Container1 = () => {
                         marginTop: '30px',
                     }}
                 >
-                    <Button onClick={() => { }} className='hireme'>
+                    <Button onClick={handleClick} className='hireme'>
                         Hire Me
                     </Button>
-                    <Button onClick={() => { }} className='Resume'>
+                    <Button onClick={() => { handleDownload() }} className='Resume'>
                         Resume <FiDownload size={13} style={{ marginLeft: 5 }} />
                     </Button>
                 </div>
@@ -61,7 +80,6 @@ const Container1 = () => {
                     <Nav.Link href={`https://www.linkedin.com/in/sri-devi-veerasamy-674aa2268`} className="nav-link-hover fw-bold me-4"><BsLinkedin size={22} /></Nav.Link>
                     <Nav.Link href={`https://wa.me/${'+917339314128'}?text=${encodeURIComponent('Hi')}`} className="nav-link-hover fw-bold me-4"><FaWhatsapp size={22} /></Nav.Link>
                     <Nav.Link href={`tel:+917339314128`} className="nav-link-hover fw-bold me-4"><MdCall size={22} /></Nav.Link>
-
                 </div>
             </div>
 
@@ -78,57 +96,22 @@ const Container1 = () => {
             >
                 <img src={require('../images/pic.png')} alt='Brand Logo' width='100%' height='auto' />
             </div>
-
+            <ToastContainer position="top-end" className="p-3">
+                <Toast
+                    onClose={() => setShowToast(false)}
+                    show={showToast}
+                    delay={3000}
+                    autohide
+                    bg="success"
+                >
+                    <Toast.Header>
+                        <strong className="me-auto">Download</strong>
+                    </Toast.Header>
+                    <Toast.Body className="text-white" >My resume is downloading...</Toast.Body>
+                </Toast>
+            </ToastContainer>
         </div>
     );
-
-
-
-
-
-
-    //  return (
-    //   <div style={{ height: 700, flexDirection: 'row', gap: 20, display: 'flex', width: '100%', paddingLeft: '40px', paddingRight: '40px', background: 'linear-gradient(to right, #BCF6D0, #BFDCFA)' }}>
-    //    {/* name container */}
-    //    <div style={{ height: '100%', width: '50%',paddingLeft:'5%' }}>
-    //     <h1 className='name'>Hello, I'm Sri Devi</h1>
-
-    //     <div className='name1'>
-    //      <Typewriter
-    //       options={{
-    //        strings: ['Fresher', 'Looking for a job'],
-    //        autoStart: true,
-    //        loop: true,
-    //       }}
-    //      />
-    //     </div>
-    //     <p>Aspiring professional ready to contribute.</p>
-
-
-    //     <div className='buttons'>
-    //      <button className='button1'>Hire Me</button>
-    //      <button className='button2'>Resume</button>
-    //     </div>
-
-    //     <div className='buttons' style={{ marginTop: 30 }}>
-    //      <Nav.Link href={`https://github.com/Sri-Devi-Veerasamy`} className="nav-link-hover fw-bold me-4"><BsGithub size={22} /></Nav.Link>
-    //      <Nav.Link href={`https://www.linkedin.com/in/sri-devi-veerasamy-674aa2268`} className="nav-link-hover fw-bold me-4"><BsLinkedin size={22} /></Nav.Link>
-    //      <Nav.Link href={`https://wa.me/${'+917339314128'}?text=${encodeURIComponent('Hi')}`} className="nav-link-hover fw-bold me-4"><FaWhatsapp size={22} /></Nav.Link>
-    //      <Nav.Link href={`tel:+917339314128`} className="nav-link-hover fw-bold me-4"><MdCall size={22} /></Nav.Link>
-
-    //     </div>
-
-
-
-
-    //    </div>
-    //    {/* image container */}
-    //    <div style={{ height: '100%', width: '50%', paddingLeft: '7%',  }}>
-    //     <img src={require('../images/pic.png')} alt="Brand Logo" width="500px" height="auto" />
-    //    </div>
-
-    //   </div>
-    //  )
 }
 
 export default Container1
